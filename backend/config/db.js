bd15bc1f406cd53d3d28ae2c4e1c49ff12df4f72
Ranjeet-K-Balobal <ranjeetbalo600@gmail.com>
2025-01-27 09:53:05 +0530

    backend set up is proper

---
+++ backend/config/db.js
@@ -0,0 +1,19 @@
+const mysql = require('mysql2');
+const dotenv = require('dotenv');
+
+// Load environment variables
+dotenv.config();
+
+// Create a connection pool
+const pool = mysql.createPool({
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+  waitForConnections: true,
+  connectionLimit: 10,
+  queueLimit: 0,
+});
+
+// Export the pool
+module.exports = pool.promise();
